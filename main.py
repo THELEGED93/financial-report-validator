32b9234dfed30f29ef4aee10ca4696bad492b9a7
Tyrone Williams <TYRONE8994@OUTLOOK.COM>
2026-05-17 09:46:42 -0400

    changes made to main.py and README.md

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ from src.validate_data import validate_data
 from src.transform_data import transform_data
 from src.generate_report import generate_report
 
-<<<<<<< HEAD
-data = load_data("data/raw_financials.csv")
-
-validated_data = validate_data(data)
-
-transformed_data = transform_data(validated_data)
-
-generate_report(transformed_data)
-=======
 file_path = "data/raw_financials.csv"
 
 df = load_data(file_path)
@@ -21,6 +12,5 @@ validation_results = validate_data(df)
 df = transform_data(df)
 
 generate_report(validation_results)
->>>>>>> 0f58be5838b9413550c1dba6622da7d2c7f383ea
 
 print("Pipeline completed successfully.")
